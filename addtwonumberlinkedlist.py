dd835c6e54e03fa5d1c2cc5172278a97cd732da7
janogelin <digicloseup@gmail.com>
2025-04-16 18:11:07 -0700

    linitial commit of inkedlist add two numbers

---
+++ addtwonumberlinkedlist.py
@@ -0,0 +1,112 @@
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class MyLinkedList:
+    def __init__(self):
+        self.size = 0
+        self.head = ListNode(0)  # sentinel node as pseudo-head
+        
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        # if index is invalid
+        if index < 0 or index >= self.size:
+            return -1
+        
+        curr = self.head
+        # index steps needed 
+        # to move from sentinel node to wanted index
+        for _ in range(index + 1):
+            curr = curr.next
+        return curr.val
+            
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self.addAtIndex(0, val)
+        
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        self.addAtIndex(self.size, val)
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        # If index is greater than the length, 
+        # the node will not be inserted.
+        if index > self.size:
+            return
+        
+        # [so weird] If index is negative, 
+        # the node will be inserted at the head of the list.
+        if index < 0:
+            index = 0
+        
+        self.size += 1
+        # find predecessor of the node to be added
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+            
+        # node to be added
+        to_add = ListNode(val)
+        # insertion itself
+        to_add.next = pred.next
+        pred.next = to_add
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        # if the index is invalid, do nothing
+        if index < 0 or index >= self.size:
+            return
+        
+        self.size -= 1
+        # find predecessor of the node to be deleted
+        pred = self.head
+        for _ in range(index):
+            pred = pred.next
+            
+        # delete pred.next 
+        pred.next = pred.next.next
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = newNode
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
+    
+
+if __name__ == '__main__':
+    llist = MyLinkedList
+
+    llist.addAtHead(3)
